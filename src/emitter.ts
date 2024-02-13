76179956055118e0a97fc5f2bc8447ae0b8dd0a7
Amit Feldman <afeldman@gaviti.com>
2024-02-13 11:11:06 +0200

    Remove console.log from emitter

--- src/emitter.ts
+++ src/emitter.ts
@@ -7,24 +7,18 @@ class Emitter {
     }
 
     this.events[event].push(listener);
-
-    console.log(`Event added: ${event}`);
   }
 
   off(event: string, listener: Function) {
     if (!this.events[event]) return;
 
     this.events[event] = this.events[event].filter((l) => l !== listener);
-
-    console.log(`Event removed: ${event}`);
   }
 
   emit(event: string, ...args: any[]) {
     if (!this.events[event]) return;
 
     this.events[event].forEach((listener) => listener(...args));
-
-    console.log(`Event emitted: ${event}`);
   }
 }
 
